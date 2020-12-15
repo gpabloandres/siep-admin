5f2ece481d8814d8cda66ddf7f9b8f3908283f54
gpabloandres <gpabloandres@gmail.com>
2020-12-15 16:58:40 -0300

    Actualiza ip.

--- webpack/config/prod.env.js
+++ webpack/config/prod.env.js
@@ -1,5 +1,5 @@
 let opt = {};
-let ingress = process.env.SIEP_API_GW_INGRESS || 'http://localhost:7777'
+let ingress = process.env.SIEP_API_GW_INGRESS || 'http://200.5.228.118:7777'
 opt.NODE_ENV = '"production"';
 opt.SIEP_API_GW_INGRESS = JSON.stringify(ingress);
 
